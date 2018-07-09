b18a516c6e05e20ccac6961dc29cc1284fb73026
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2018-07-08 21:51:08 -0300

    Creating evaluators for standard numeric types

--- src/SimilarityToolkit.Tests/EvaluatorsTests.fs
+++ src/SimilarityToolkit.Tests/EvaluatorsTests.fs
@@ -17,6 +17,54 @@ let ``SByteSimilarityEvaluator: distance equals absolute value of the difference
     SByteSimilarityEvaluator().EvaluateDistance(x, y) = expected
 
 [<Property>]
+let ``Int16SimilarityEvaluator: distance equals absolute value of the difference`` (x : int16) (y : int16) =
+    let expected = Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
+    Int16SimilarityEvaluator().EvaluateDistance(x, y) = expected
+
+[<Property>]
+let ``UInt16SimilarityEvaluator: distance equals absolute value of the difference`` (x : uint16) (y : uint16) =
+    let expected = Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
+    UInt16SimilarityEvaluator().EvaluateDistance(x, y) = expected
+
+[<Property>]
+let ``UInt32SimilarityEvaluator: distance equals absolute value of the difference`` (x : uint32) (y : uint32) =
+    let expected = Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
+    UInt32SimilarityEvaluator().EvaluateDistance(x, y) = expected
+
+[<Property>]
+let ``Int32SimilarityEvaluator: distance equals absolute value of the difference`` (x : int) (y : int) =
+    let expected = Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
+    Int32SimilarityEvaluator().EvaluateDistance(x, y) = expected
+
+[<Property>]
+let ``Int64SimilarityEvaluator: distance equals absolute value of the difference`` (x : int64) (y : int64) =
+    let expected = Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
+    Int64SimilarityEvaluator().EvaluateDistance(x, y) = expected
+
+[<Property>]
+let ``UInt64SimilarityEvaluator: distance equals absolute value of the difference`` (x : uint64) (y : uint64) =
+    let expected = Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
+    UInt64SimilarityEvaluator().EvaluateDistance(x, y) = expected
+
+[<Property>]
+let ``SingleSimilarityEvaluator: distance equals absolute value of the difference`` (x : single) (y : single) =
+    let evaluator = SingleSimilarityEvaluator()
+    try
+        let expected = x - y |> Convert.ToDecimal |> Math.Abs
+        evaluator.EvaluateDistance(x, y) = expected
+    with
+    | :? OverflowException -> evaluator.EvaluateDistance(x, y) = Decimal.MaxValue
+
+[<Property>]
+let ``DoubleSimilarityEvaluator: distance equals absolute value of the difference`` (x : double) (y : double) =
+    let evaluator = DoubleSimilarityEvaluator()
+    try
+        let expected = x - y |> Convert.ToDecimal |> Math.Abs
+        evaluator.EvaluateDistance(x, y) = expected
+    with
+    | :? OverflowException -> evaluator.EvaluateDistance(x, y) = Decimal.MaxValue
+
+[<Property>]
 let ``DecimalSimilarityEvaluator: distance equals absolute value of the difference`` (x : decimal) (y : decimal) =
     let evaluator = DecimalSimilarityEvaluator()
     try
@@ -28,6 +76,14 @@ let ``DecimalSimilarityEvaluator: distance equals absolute value of the differen
 [<Fact>]
 let ``DecimalSimilarityEvaluator: if difference causes an overflow, return decimal's max value`` () =
     DecimalSimilarityEvaluator().EvaluateDistance(Decimal.MaxValue, -1M) |> equals Decimal.MaxValue
+
+[<Fact>]
+let ``SingleSimilarityEvaluator: if difference causes an overflow, return decimal's max value`` () =
+    SingleSimilarityEvaluator().EvaluateDistance(Single.MaxValue, -1.0f) |> equals Decimal.MaxValue
+
+[<Fact>]
+let ``DoubleSimilarityEvaluator: if difference causes an overflow, return decimal's max value`` () =
+    DoubleSimilarityEvaluator().EvaluateDistance(Double.MaxValue, -1.0) |> equals Decimal.MaxValue
 
 [<Property>]
 let ``DateTimeSimilarityEvaluator: distance equals difference in days or seconds`` (x : DateTime) (y : DateTime) =
--- src/SimilarityToolkit/Evaluators.fs
+++ src/SimilarityToolkit/Evaluators.fs
@@ -13,6 +13,52 @@ type SByteSimilarityEvaluator() =
     override __.EvaluateDistance(x : sbyte, y : sbyte) =
         Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
 
+type Int16SimilarityEvaluator() =
+    inherit SimilarityEvaluatorBase<int16>()
+    override __.EvaluateDistance(x : int16, y : int16) =
+        Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
+
+type UInt16SimilarityEvaluator() =
+    inherit SimilarityEvaluatorBase<uint16>()
+        override __.EvaluateDistance(x : uint16, y : uint16) =
+            Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
+
+type UInt32SimilarityEvaluator() =
+    inherit SimilarityEvaluatorBase<uint32>()
+        override __.EvaluateDistance(x : uint32, y : uint32) =
+            Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
+
+type Int32SimilarityEvaluator() =
+    inherit SimilarityEvaluatorBase<int>()
+    override __.EvaluateDistance(x : int, y : int) =
+        Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
+
+type Int64SimilarityEvaluator() =
+    inherit SimilarityEvaluatorBase<int64>()
+        override __.EvaluateDistance(x : int64, y : int64) =
+            Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
+
+type UInt64SimilarityEvaluator() =
+    inherit SimilarityEvaluatorBase<uint64>()
+        override __.EvaluateDistance(x : uint64, y : uint64) =
+            Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
+
+type SingleSimilarityEvaluator() =
+    inherit SimilarityEvaluatorBase<single>()
+        override __.EvaluateDistance(x : single, y : single) =
+            try
+                x - y |> Convert.ToDecimal |> Math.Abs
+            with
+            | :? OverflowException -> Decimal.MaxValue
+
+type DoubleSimilarityEvaluator() =
+    inherit SimilarityEvaluatorBase<double>()
+        override __.EvaluateDistance(x : double, y : double) =
+            try
+                x - y |> Convert.ToDecimal |> Math.Abs
+            with
+            | :? OverflowException -> Decimal.MaxValue
+
 type DateTimeSimilarityEvaluator() =
     inherit SimilarityEvaluatorBase<DateTime>()
     override __.EvaluateDistance(x : DateTime, y : DateTime) =
