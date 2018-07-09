ffbac456bf22bc24e51f663e4bbfbf057017126d
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2018-07-09 01:23:43 -0300

    Creating nullable evaluator, default primitive evaluators

--- src/SimilarityToolkit/Abstractions.fs
+++ src/SimilarityToolkit/Abstractions.fs
@@ -14,3 +14,10 @@ type SimilarityEvaluatorBase<'T>() =
     override this.EvaluateDistance(x : obj, y : obj) =
         this.EvaluateDistance(x :?> 'T, y :?> 'T)
     override __.EvaluatedType = typeof<'T>
+
+type NullableSimilarityEvaluator<'T when 'T : struct and 'T : (new : unit -> 'T) and 'T :> ValueType>(evaluator : SimilarityEvaluatorBase<'T>) =
+    inherit SimilarityEvaluatorBase<Nullable<'T>>()
+    override __.EvaluateDistance(x : Nullable<'T>, y : Nullable<'T>) =
+        let x = box <| if x.HasValue then x.Value else Unchecked.defaultof<'T>
+        let y = box <| if y.HasValue then y.Value else Unchecked.defaultof<'T>
+        evaluator.EvaluateDistance(x, y)
--- src/SimilarityToolkit/Evaluators.fs
+++ src/SimilarityToolkit/Evaluators.fs
@@ -2,48 +2,85 @@
 
 open System
 open SimilarityToolkit.Abstractions
+open System.Collections.ObjectModel
+open System.Collections.Generic
+open System.Linq
 
-type ByteSimilarityEvaluator() =
+type SimilarityEvaluatorCollection private (list) =
+    inherit ReadOnlyCollection<SimilarityEvaluatorBase>(list)
+    static member From(evaluators : IEnumerable<SimilarityEvaluatorBase>) =
+        SimilarityEvaluatorCollection(evaluators.ToList())
+    static member Default =
+        let buildvn x : SimilarityEvaluatorBase * SimilarityEvaluatorBase =
+            upcast x, upcast NullableSimilarityEvaluator<'T>(x)
+        let listvn x =
+            x |> List.fold (fun acc (x, y) -> x :: y :: acc) []
+        let primitives =
+            [ buildvn <| ByteSimilarityEvaluator()
+              buildvn <| SByteSimilarityEvaluator()
+              buildvn <| Int16SimilarityEvaluator()
+              buildvn <| UInt16SimilarityEvaluator()
+              buildvn <| Int32SimilarityEvaluator()
+              buildvn <| UInt32SimilarityEvaluator()
+              buildvn <| Int64SimilarityEvaluator()
+              buildvn <| UInt64SimilarityEvaluator()
+              buildvn <| SingleSimilarityEvaluator()
+              buildvn <| DoubleSimilarityEvaluator()
+              buildvn <| DateTimeSimilarityEvaluator()
+              buildvn <| DecimalSimilarityEvaluator() ]
+            |> listvn
+            |> List.append [ StringSimilarityEvaluator() ]
+        SimilarityEvaluatorCollection.From(primitives)
+
+and SimilarityEvaluatorDictionary private (dictionary) =
+    inherit ReadOnlyDictionary<Type, SimilarityEvaluatorBase>(dictionary)
+    static member From(evaluators : IEnumerable<SimilarityEvaluatorBase>) =
+        let dict = evaluators.ToDictionary(fun k -> k.EvaluatedType)
+        SimilarityEvaluatorDictionary(dict)
+    static member Default =
+        SimilarityEvaluatorDictionary.From(SimilarityEvaluatorCollection.Default)
+
+and ByteSimilarityEvaluator() =
     inherit SimilarityEvaluatorBase<byte>()
     override __.EvaluateDistance(x : byte, y : byte) =
         Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
 
-type SByteSimilarityEvaluator() =
+and SByteSimilarityEvaluator() =
     inherit SimilarityEvaluatorBase<sbyte>()
     override __.EvaluateDistance(x : sbyte, y : sbyte) =
         Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
 
-type Int16SimilarityEvaluator() =
+and Int16SimilarityEvaluator() =
     inherit SimilarityEvaluatorBase<int16>()
     override __.EvaluateDistance(x : int16, y : int16) =
         Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
 
-type UInt16SimilarityEvaluator() =
+and UInt16SimilarityEvaluator() =
     inherit SimilarityEvaluatorBase<uint16>()
         override __.EvaluateDistance(x : uint16, y : uint16) =
             Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
 
-type UInt32SimilarityEvaluator() =
+and UInt32SimilarityEvaluator() =
     inherit SimilarityEvaluatorBase<uint32>()
         override __.EvaluateDistance(x : uint32, y : uint32) =
             Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
 
-type Int32SimilarityEvaluator() =
+and Int32SimilarityEvaluator() =
     inherit SimilarityEvaluatorBase<int>()
     override __.EvaluateDistance(x : int, y : int) =
         Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
 
-type Int64SimilarityEvaluator() =
+and Int64SimilarityEvaluator() =
     inherit SimilarityEvaluatorBase<int64>()
         override __.EvaluateDistance(x : int64, y : int64) =
             Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
 
-type UInt64SimilarityEvaluator() =
+and UInt64SimilarityEvaluator() =
     inherit SimilarityEvaluatorBase<uint64>()
         override __.EvaluateDistance(x : uint64, y : uint64) =
             Convert.ToDecimal(x) - Convert.ToDecimal(y) |> Math.Abs
 
-type SingleSimilarityEvaluator() =
+and SingleSimilarityEvaluator() =
     inherit SimilarityEvaluatorBase<single>()
         override __.EvaluateDistance(x : single, y : single) =
             try
@@ -51,7 +88,7 @@ type SingleSimilarityEvaluator() =
             with
             | :? OverflowException -> Decimal.MaxValue
 
-type DoubleSimilarityEvaluator() =
+and DoubleSimilarityEvaluator() =
     inherit SimilarityEvaluatorBase<double>()
         override __.EvaluateDistance(x : double, y : double) =
             try
@@ -59,14 +96,14 @@ type DoubleSimilarityEvaluator() =
             with
             | :? OverflowException -> Decimal.MaxValue
 
-type DateTimeSimilarityEvaluator() =
+and DateTimeSimilarityEvaluator() =
     inherit SimilarityEvaluatorBase<DateTime>()
     override __.EvaluateDistance(x : DateTime, y : DateTime) =
         if x.Date = x && y.Date = y
         then (x - y).TotalDays |> Convert.ToDecimal
         else (x - y).TotalSeconds |> Convert.ToDecimal
 
-type DecimalSimilarityEvaluator() =
+and DecimalSimilarityEvaluator() =
     inherit SimilarityEvaluatorBase<decimal>()
     override __.EvaluateDistance(x : decimal, y : decimal) =
         try
@@ -74,7 +111,7 @@ type DecimalSimilarityEvaluator() =
         with
         | :? OverflowException -> Decimal.MaxValue
 
-type StringSimilarityEvaluator() =
+and StringSimilarityEvaluator() =
     inherit SimilarityEvaluatorBase<string>()
         override __.EvaluateDistance(x : string, y : string) =
             let length (x : string) =
